f414ddcb501e8825d61b05be6e86b3cb64c4a851
Abhigyan Shukla <ayush1abhigyan@gmail.com>
2026-07-20 00:50:41 +0530

    Add Divisibility [J] from Codeforces

---
+++ 630/J - Divisibility/solution.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long long x;
+    cin>>x;
+    long long y = x/2520;
+    cout<<y;
+ 
+    return 0;
+}
