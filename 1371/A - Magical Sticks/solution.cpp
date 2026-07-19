8f9a8ec56e9665908d695f7a940339ac91afaa70
Abhigyan Shukla <ayush1abhigyan@gmail.com>
2026-07-20 00:41:56 +0530

    Add Magical Sticks [A] from Codeforces

---
+++ 1371/A - Magical Sticks/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int a;
+  cin>>a;
+  while(a--){
+    int n;
+    cin>>n;
+    cout<<(n+1)/2<<'
+';
+  }
+  return 0;
+}
