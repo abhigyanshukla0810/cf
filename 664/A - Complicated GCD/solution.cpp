03e74d8031a1813b88af969ff5cf139eaa2156f0
Abhigyan Shukla <ayush1abhigyan@gmail.com>
2026-07-20 00:49:24 +0530

    Add Complicated GCD [A] from Codeforces

---
+++ 664/A - Complicated GCD/solution.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s1,s2;
+    cin>>s1>>s2;
+    if(s1==s2) cout<<s1;
+    else cout<<1;
+    return 0;
+}
