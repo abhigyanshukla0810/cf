960adb5d36535f4a94ee570351d5b632b5b206d1
Abhigyan Shukla <ayush1abhigyan@gmail.com>
2026-07-20 00:33:45 +0530

    Add Bear and Big Brother [A] from Codeforces

---
+++ 791/A - Bear and Big Brother/solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,i;
+    cin>>a>>b;
+    i=0;
+    while (a<=b){
+        a = a*3;
+        b = b*2;
+        i++;
+    }
+    cout<<i;
+    return 0;
+}
