90668002dd5cd80fb53e57ec48b54dfaef3f82e8
Abhigyan Shukla <ayush1abhigyan@gmail.com>
2026-07-20 00:37:05 +0530

    Add A Simple Sequence [A] from Codeforces

---
+++ 2210/A - A Simple Sequence/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    while(n--){
+        int a;
+        cin>>a;
+    for(int i = a; i>0;i--){
+        cout<<i<<" ";
+    }
+}
+    return 0;
+}
