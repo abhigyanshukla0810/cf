244ef10271afe2e0afcd566259843823d8bb1fe2
Abhigyan Shukla <ayush1abhigyan@gmail.com>
2026-07-20 00:50:49 +0530

    Add Game [R] from Codeforces

---
+++ 630/R - Game/solution.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long long x;
+    cin>>x;
+    if(x&1) cout<<1;
+    else cout<<2;
+    return 0;
+}
