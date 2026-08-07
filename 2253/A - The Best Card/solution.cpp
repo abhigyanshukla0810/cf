94c3d11ce27624a28c156a1b57205452b156a824
Abhigyan Shukla <ayush1abhigyan@gmail.com>
2026-08-07 20:23:18 +0530

    Add The Best Card [A] from Codeforces

---
+++ 2253/A - The Best Card/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+bool isPrime(int n) {
+    if (n < 2) return false;     
+    if (n == 2) return true;     
+    if (n % 2 == 0) return false;  
+    for (int i = 3; i <= sqrt(n); i += 2) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+ 
+int main() {
+	int t;
+	cin >> t;
+	while(t --) {
+	    int n;
+	    cin>>n;
+	    if(isPrime(n+1)) cout<<"YES"<<'
+';
+	    else cout<<"NO"<<'
+';
+	}
+    return 0;
+}
