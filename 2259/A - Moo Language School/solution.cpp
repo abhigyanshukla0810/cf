19351778563dcec8984e8bbceeb1a303d5edd403
Abhigyan Shukla <ayush1abhigyan@gmail.com>
2026-09-06 00:51:56 +0530

    Add Moo Language School [A] from Codeforces

---
+++ 2259/A - Moo Language School/solution.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        string s;
+        cin>>s;
+        int ans=0;
+        for(int i=0;i<n;i+=k){
+            bool zero=false;
+            for(int j=i;j<i+k;j++){
+                if(s[j]=='0'){
+                    zero=true;
+                    break;
+                }
+            }
+            if(!zero) ans++;
+        }
+        cout<<ans<<'
+';
+    }
+    return 0;
+}
