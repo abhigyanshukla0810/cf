45a029448467baff80dcf4dd13c48ce8e2b7b33d
Abhigyan Shukla <ayush1abhigyan@gmail.com>
2026-09-12 11:17:37 +0530

    Add Harry Potter and Three Spells [A] from Codeforces

---
+++ 65/A - Harry Potter and Three Spells/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    float a,b,c,d,e,f;
+    cin>>a>>b>>c>>d>>e>>f;
+    if(d && (!c || (b && (!a || (!e && f))))) cout << "Ron";
+    else{
+        float x = b/a;
+        float y = (d/c)*x;
+        float z = (f/e)*y;
+        if(1<z)cout<<"Ron";
+        else cout<<"Hermione";
+    }
+    return 0;
+}
