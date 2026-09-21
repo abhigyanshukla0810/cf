24488466a4b20618785b1a4c2487cc6e7a987157
Abhigyan Shukla <ayush1abhigyan@gmail.com>
2026-09-21 20:25:55 +0530

    Add Three Piles [B] from Codeforces

---
+++ 2266/B - Three Piles/solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);   
+    int t;
+    cin>>t;
+    while(t--){
+        long long a,b,c;
+        cin>>a>>b>>c;
+        long long s = max(abs(b-a),abs(a+c-b));
+        cout<<s<<'
+';
+    }
+    return 0;
+}
