9b9113f8f0d611cabaa7e7706f0735ea1e74ec26
Abhigyan Shukla <ayush1abhigyan@gmail.com>
2026-09-27 00:52:42 +0530

    Add SauSaGe Bank [A] from Codeforces

---
+++ 2269/A - SauSaGe Bank/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);   
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        cout<<(1LL<<(n-k+1)) + (2LL*(k-1))<<'
+';
+    }
+    return 0;
+}
